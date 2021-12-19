46ec85939d88748fe71454079b9327512184ab95
haderman <hadermania@gmail.com>
2021-12-19 15:23:13 -0500

    upgrade packages

--- src/components/buy-coffee/buy-coffee.component.tsx
+++ src/components/buy-coffee/buy-coffee.component.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components'
 
-// import './stack.css';
-
 export default function BuyCoffee() {
   return (
     <Container>buy me a coffee</Container>
